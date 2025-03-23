3e23d4fee41f3d1b453eff4ac2110da096de3e19
Raí <devvandenberg>
2025-03-22 21:53:34 -0300

    Validação e tratamento de error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+
+import { errorHandling } from './middlewares/error-handling';
+import { z } from "zod"
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
+    const BodySchema = z.object({
+        age: z.number().min(18),
+    })
+
+    const { age } = BodySchema.parse(req.body);
     res.send("Hello World!");
 })
+
+app.use(errorHandling);
 
 export { app }
