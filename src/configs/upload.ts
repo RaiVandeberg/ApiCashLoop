e7daeb01df90ff972122d0fc376325d79a73b0f5
Raí <devvandenberg>
2025-03-25 23:02:24 -0300

    validando arquivos

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,10 +5,12 @@ import crypto from "node:crypto";
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 const UPLOAS_FOLDER = path.relative(TMP_FOLDER, "uploads");
 
+// 1KB = 1024 bytes
+// 1MB = 1024 * 1024 * X 
+const MAX_SIZE = 3
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE; // 3MB
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3; // 3MB
-
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/jpg"];
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/jpg", "application/pdf"];
 
 const MULTER = {
     storage: multer.diskStorage({
@@ -27,5 +29,6 @@ export default{
     UPLOAS_FOLDER, 
     MAX_FILE_SIZE, 
     ACCEPTED_IMAGE_TYPES, 
-    MULTER 
+    MULTER,
+    MAX_SIZE
 }
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
+
+import uploadConfig from "@/configs/upload"
+import { AppError } from '@/utils/AppError';
 
 
 class UploadsController { 
     async create(request:Request, response:Response) {
-        response.json({message: "Uploads Controller"})
+        
+        try {
+            const fileSchema = z.object({
+                filename:z.string().min(1, {message:"Arquivo é obrigatório"}),
+                mimetype:z.string().refine((type => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type)), {message:"Tipo de arquivo não suportado, Formatos permitidos: " + uploadConfig.ACCEPTED_IMAGE_TYPES}),
+                size: z.number().positive().refine((size) => size <= uploadConfig.MAX_FILE_SIZE, {message: " Tamnho permitido de até " + uploadConfig.MAX_SIZE} )          
+            }).passthrough()
+
+            const { file } = fileSchema.parse(request.file)
+
+            response.json({message: "ok"})
+        } catch (error) {
+            throw error
+        }
     }
 }
 
